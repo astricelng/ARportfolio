97666e3376a25894963addce8460066697efc729
Astrid Narvaez <astricelng@gmail.com>
2022-09-05 13:45:30 -0500

    world tracking

--- world_tracking/main.js
+++ world_tracking/main.js
@@ -13,18 +13,13 @@ document.addEventListener("DOMContentLoaded", () => {
     let selectedItem = null;
     let touchDown = false;
     let prevTouchPosition = null;
+    let placeButton = document.querySelector("#confirm-buttons #place");
+    let cancelButton = document.querySelector("#confirm-buttons #cancel");
+    let itemButton = document.querySelectorAll(".item-button");
+    const itemNames = ["coffee-table", "chair", "cushion"];
 
     const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     scene.add(light);
-
-    /*const reticleGeometry = new THREE.RingGeometry(0.15, 0.2, 32).rotateX(
-      -Math.PI / 2
-    );
-    const reticleMaterial = new THREE.MeshBasicMaterial();
-    const reticle = new THREE.Mesh(reticleGeometry, reticleMaterial);
-    reticle.matrixAutoUpdate = false;
-    reticle.visible = false;
-    scene.add(reticle);*/
 
     const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -39,14 +34,55 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.appendChild(renderer.domElement);
     document.body.appendChild(arButton);
 
-    const items = await addItems(scene);
+    const items = await addItems(itemNames, scene);
 
     const select = (selectItem) => {
+      if (selectedItem === selectItem) return;
+
+      for (var i = 0, m = items.length; i < m; i++) {
+        items[i].visible = selectItem === items[i];
+      }
+
       selectedItem = selectItem;
-      alert("1 ", selectedItem);
     };
 
-    select(items[0]);
+    const cancelSelect = () => {
+      if (selectedItem) selectedItem.visible = false;
+
+      selectedItem = null;
+    };
+
+    // LISTENER INTERFACE BUTTONS
+    itemButton.forEach((button) => {
+      button.addEventListener("click", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        let indexItem = itemNames.indexOf(button.id);
+        if (indexItem > -1) select(items[indexItem]);
+      });
+    });
+
+    placeButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      //clonarlo y posicionarlo
+      const spawnItem = deepClone(selectedItem);
+      setOpacity(spawnItem, 1.0);
+      scene.add(spawnItem);
+
+      cancelSelect();
+    });
+
+    cancelButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      cancelSelect();
+    });
+
+    // END LISTENER INTERFACE BUTTONS
 
     const controller = renderer.xr.getController(0);
     scene.add(controller);
@@ -55,7 +91,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     controller.addEventListener("selectend", (e) => {
       touchDown = false;
-      //prevTouchPosition = null;
+      prevTouchPosition = null;
     });
     /*controller.addEventListener("select", () => {
       const geometry = new THREE.BoxGeometry(0.06, 0.06, 0.06);
@@ -86,6 +122,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const referenceSpace = renderer.xr.getReferenceSpace();
 
+        // Para rotar el item cuando el usuario hace touch sobre el
         if (touchDown && selectedItem) {
           const viewerMatrix = new THREE.Matrix4().fromArray(
             frame.getViewerPose(referenceSpace).transform.inverse.matrix
@@ -97,9 +134,11 @@ document.addEventListener("DOMContentLoaded", () => {
             const deltaZ = newPosition.y - prevTouchPosition.y;
             selectedItem.rotation.y += deltaX * 30;
           }
+
           prevTouchPosition = newPosition;
         }
 
+        // Para pintar el elemento seleccionado
         if (selectedItem) {
           const hitTestResults = frame.getHitTestResults(hitTestSource);
 
@@ -107,15 +146,11 @@ document.addEventListener("DOMContentLoaded", () => {
             const hit = hitTestResults[0];
             const hitPose = hit.getPose(referenceSpace);
 
-            /*reticle.visible = true;
-            reticle.matrix.fromArray(hitPose.transform.matrix);*/
-
             selectedItem.visible = true;
             selectedItem.position.setFromMatrixPosition(
               new THREE.Matrix4().fromArray(hitPose.transform.matrix)
             );
           } else {
-            //reticle.visible = false;
             selectedItem.visible = false;
           }
         }
@@ -142,8 +177,29 @@ const normalizeModel = (obj, height) => {
   obj.position.set(-center.x, -center.y, -center.z);
 };
 
-const addItems = async (scene) => {
-  const itemNames = ["coffee-table", "chair", "cushion"];
+// make clone object not sharing materials
+const deepClone = (obj) => {
+  const newObj = obj.clone();
+  newObj.traverse((o) => {
+    if (o.isMesh) {
+      o.material = o.material.clone();
+    }
+  });
+  return newObj;
+};
+
+// recursively set opacity
+const setOpacity = (obj, opacity) => {
+  obj.children.forEach((child) => {
+    setOpacity(child, opacity);
+  });
+  if (obj.material) {
+    obj.material.format = THREE.RGBAFormat; // required for opacity
+    obj.material.opacity = opacity;
+  }
+};
+
+const addItems = async (itemNames, scene) => {
   const itemHeights = [0.3, 0.7, 0.05];
   const items = [];
   for (let i = 0; i < itemNames.length; i++) {
@@ -154,10 +210,10 @@ const addItems = async (scene) => {
     const item = new THREE.Group();
     item.add(model.scene);
     item.visible = false;
-    //setOpacity(item, 0.5);
+    setOpacity(item, 0.3);
     items.push(item);
     scene.add(item);
   }
-
+  alert("ITEMS");
   return items;
 };
